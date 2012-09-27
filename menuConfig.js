24512bff90c97ca2490c9a8ec7330983c6192065
William Skidmore <tony.skidmore@sage.com>
2012-09-27 16:22:08 -0700

    initial v3.0 docs

---
+++ menuConfig.js
@@ -0,0 +1,9 @@
+var menuConfig = [
+    {
+        text: 'Argos SDK v2.0 <span class="label label-stable">Stable</span>',
+        href: 'http://sage.github.com/argos/'
+    },{
+        text: 'Argos SDK v3.0 <span class="label label-dev">Dev</span>',
+        href: 'http://sage.github.com/argos/v3/'
+    }
+];
